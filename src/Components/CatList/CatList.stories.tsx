2ede198ed6a785974f662b8bccc5fb75adde533e
Diogo DE ARAUJO <16623572+DigoArj@users.noreply.github.com>
2020-06-21 21:46:30 +0200

    fixed stories not running correctly due to adding AppProvider

--- src/Components/CatList/CatList.stories.tsx
+++ src/Components/CatList/CatList.stories.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { CatList } from 'Components';
+import { AppProvider } from 'Stores';
 
 export default {
   title: 'Components/CatList',
   component: CatList,
 };
 
-export const Simple: React.FC = () => <CatList />;
+export const Simple: React.FC = () => (
+  <AppProvider>
+    <CatList />
+  </AppProvider>
+);
--- src/Components/Scores/Scores.stories.tsx
+++ src/Components/Scores/Scores.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Scores } from 'Components';
 import { Route, MemoryRouter, useLocation } from 'react-router-dom';
 import { StoryFn } from '@storybook/addons';
+import { AppProvider } from 'Stores';
 
 export default {
   title: 'Components/Scores',
@@ -10,11 +11,13 @@ export default {
   decorators: [
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     (storyFn: StoryFn) => (
-      <MemoryRouter initialEntries={['/']}>
-        {/*
+      <AppProvider>
+        <MemoryRouter initialEntries={['/']}>
+          {/*
         // @ts-ignore */}
-        <Route path="/" component={() => storyFn()} />
-      </MemoryRouter>
+          <Route path="/" component={() => storyFn()} />
+        </MemoryRouter>
+      </AppProvider>
     ),
   ],
 };
