2e0fae5ae245ecb67f2523b615682c13d7970829
DigoArj <DigoArj@users.noreply.github.com>
2020-06-21 21:46:19 +0000

    deploy: 33119dc635f2ee35ad2bb906ed0ea29acb92e353

---
+++ precache-manifest.584a77ca116cc7be9042a02111828f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691bce890970072ac0d5a559089c1c94",
+    "url": "/catmash/index.html"
+  },
+  {
+    "revision": "cf41af1f099c44f7efac",
+    "url": "/catmash/static/js/2.7539c53c.chunk.js"
+  },
+  {
+    "revision": "0bfeee22f3e4274f5e9f61c34a50af29",
+    "url": "/catmash/static/js/2.7539c53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdf025043e9de515d77",
+    "url": "/catmash/static/js/main.865e03d9.chunk.js"
+  },
+  {
+    "revision": "5306278ba47f6a77f317",
+    "url": "/catmash/static/js/runtime-main.f96a609e.js"
+  }
+]);
